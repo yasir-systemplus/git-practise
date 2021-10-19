734cdc58d3974622a29da7e52a107d151ca76035
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-19 07:59:27 +0500

    Final Counters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
 import CountersPage from "./components/CountersPage";
+import Foo from "./components/Foo";
 
 function App() {
+  const [visible, setVisible] = useState(true);
+
+  // <span>
+  //     <button onClick={() => setVisible((state) => !state)}>Toggle</button>
+  //     {visible && <Foo />}
+  //   </span>
   return <CountersPage />;
 }
 
--- src/components/Foo.js
+++ src/components/Foo.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
 
 export default class Foo extends Component {
+  state = { value: 1 };
   constructor(props) {
     //super is used to call the constructor of component to get some functionality.
     super(props);
+
+    console.log("Contrusctor");
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    console.log("Click happened");
-    console.log(this);
+    this.setState({ value: 2 });
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentWillUpdate() {
+    console.log("componentWillUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
   }
 
   render() {
+    console.log("render");
+
     //if a function is called from another function,
     //it will point to the window/global/undefined window.
 
     return <button onClick={this.handleClick}>Click Me</button>;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
   }
 }
